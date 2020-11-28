e6b389d5fe20afe6620599a3d09dd0005a505e8e
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-11-28 17:49:04 +0200

    handle fetch error

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect }from 'react';
 
 const Home = () => {
     const [response, setResponse] = useState('')
+    const [err, setErr] = useState('')
     
     useEffect(() => {
         fetchResponse()
@@ -10,13 +11,16 @@ const Home = () => {
     const fetchResponse = async () => {
         const res = await fetch('http://127.0.0.1:5000', {
             method: 'GET'
-        });
-        const jsonData = await res.json();
-        setResponse(jsonData.message)
+        }).catch((error) => setErr(error.name + ': ' + error.message))
+        console.log(err)
+        if (err !== ''){
+            const jsonData = await res.json();
+            setResponse(jsonData.message)      
+        }
     }
 
     return (  
-        <>
+        <>  
             <h1>{response}</h1>
         </>
     );
