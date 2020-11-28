c9e9854454a4a7f4dd1f1daa90c29ca970c21615
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-11-28 18:18:56 +0200

    update fetch

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,25 +3,25 @@ import React, { useState, useEffect }from 'react';
 const Home = () => {
     const [response, setResponse] = useState('')
     const [err, setErr] = useState('')
-    
+
     useEffect(() => {
         fetchResponse()
-    },[])
+    });
 
     const fetchResponse = async () => {
-        const res = await fetch('http://127.0.0.1:5000', {
+        await fetch('http://127.0.0.1:5000', {
             method: 'GET'
-        }).catch((error) => setErr(error.name + ': ' + error.message))
-        console.log(err)
-        if (err !== ''){
-            const jsonData = await res.json();
-            setResponse(jsonData.message)      
-        }
+        }).then((res) => res.json())
+        .then((res) => setResponse(res.message))
+        .catch((error) => {
+            setErr(error.name + ': ' + error.message);
+            console.log(err)
+        })            
     }
 
     return (  
         <>  
-            <h1>{response}</h1>
+            { (err === '') ? <h1>{response}</h1> : <h1>{err}</h1>}
         </>
     );
 }
